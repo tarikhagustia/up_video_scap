7b58c38018da7b7994cb830e0a066f4b12fff148
Tarikh Agustia Ijudin <agustia.tarikh150@gmail.com>
2020-08-05 08:56:18 +0700

    Add Instagram

--- routes/instagram.js
+++ routes/instagram.js
@@ -6,7 +6,7 @@ const Instagram = require('instagram-web-api')
 const { IG_USERNAME, IG_PASSWORD } = process.env
 const FileCookieStore = require('tough-cookie-filestore2')
 const cookieStore = new FileCookieStore('./storage/cookies.json')
-const client = new Instagram({ username : IG_USERNAME, password: IG_PASSWORD, cookieStore })
+const client = new Instagram({ username: IG_USERNAME, password: IG_PASSWORD, cookieStore })
 
 /**
  * Get Trending Videos  
@@ -21,7 +21,7 @@ router.get('/search', async (req, res, next) => {
 /**
  * Get Media Feed By Hashtag
  */
-router.get('/media/hashtag', async (req, res, next) => {
+router.get('/media/hashtag', cache(60 * 2), async (req, res, next) => {
   const { hashtag } = req.query
   await client.login()
   const media = await client.getMediaFeedByHashtag({ hashtag: hashtag })
@@ -31,11 +31,21 @@ router.get('/media/hashtag', async (req, res, next) => {
 /**
  * Get Short Code
  */
-router.get('/media/:shortcode', async (req, res, next) => {
+router.get('/media/:shortcode', cache(60 * 2), async (req, res, next) => {
   const { shortcode } = req.params
   await client.login()
   const media = await client.getMediaByShortcode({ shortcode })
   res.send(media)
+});
+
+router.get('/facebook/downloader', cache(60 * 2), async (req, res, next) => {
+  const { url } = req.query
+
+  require('fb-video-downloader')
+    .getInfo(url)
+    .then((info) => {
+      res.send(info)
+    }).catch(() => res.status(500).send({message : "Error"}));
 });
 
 
