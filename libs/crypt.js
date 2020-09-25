7e5b12d5c25bfcd975a8d59ea7735f1f6440bfad
Tarikh Agustia <agustia.tarikh150@gmail.com>
2020-09-25 13:46:42 +0700

    cryp library

---
+++ libs/crypt.js
@@ -0,0 +1,18 @@
+const crypto = require('crypto');
+const KEY = "2dsXds23049kcmfpda";
+
+
+
+function encode(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+       result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+
+module.exports = {
+    encode
+}
